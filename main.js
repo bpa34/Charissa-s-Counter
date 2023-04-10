b85022c5b38b27b44ad5d65df5b8565776f542fd
bpa34 <brett.atkinson@me.com>
2023-04-10 10:33:42 -0700

    inital commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const userInput = document.getElementById('userInput');
+const characterCounter = document.getElementById('characterCountNumber');
+const wordCounter = document.getElementById('wordCounter');
+const sentenceCounter = document.getElementById('sentenceCounter');
+
+function countCharacters() {
+  const currentLength = userInput.value.length;
+  characterCounter.innerHTML = currentLength;
+}
+
+function countWords() {
+  const inputWords = userInput.value.match(/\S+/g);
+  wordCounter.innerHTML = inputWords ? inputWords.length : 0;
+}
+
+function countSentences() {
+  const inputSentences = userInput.value.split(/[.?!]+/);
+  const nonEmptySentences = inputSentences.filter(sentence => sentence.trim().length > 0);
+  sentenceCounter.innerHTML = nonEmptySentences.length;
+}
+
+userInput.addEventListener('input', () => {
+  countCharacters();
+  countWords();
+  countSentences();
+});
